0b27a9dff302019551579ab34aede743cdc874bd
SteelPlated <mix.singatulin@yandex.ru>
2022-10-05 13:09:39 +0500

    1

--- OOP/Service.java
+++ OOP/Service.java
@@ -3,12 +3,16 @@ package OOP;
 public class Service {
     public static void main(String[]args){
         VendingMachine vm=new VendingMachine();
-        System.out.println(vm.getProductByName("Doshirac"));
+        System.out.println("-------по цене--------");
+        vm.getProductByCost(10.00);
+        System.out.println("-------по названию--------");
+        vm.getProductByName("Mr.Brown");
+      /*  System.out.println(vm.getProductByName("Doshirac"));
         System.out.println(vm.getProductByName("snickers"));
         System.out.println(vm.getBotlleOfTea("aqua", 10.0, 0.5));
         System.out.println(vm.getCookieeeee("nyam", 10.0, 0.25));
         System.out.println(vm.getChocolate("Mr.Brown", 10.0, "srawberry"));
-
+*/
 
 
     }
--- OOP/VendingMachine.java
+++ OOP/VendingMachine.java
@@ -19,22 +19,28 @@ public class VendingMachine {
     public VendingMachine (){
         products.add (new Product("Doshirac",40.0));
         products.add (new Product("snickers",30.0));
+        products.add (new BotlleOfTea("aqua", 10.0, 0.5));
+        products.add (new Cookieeeee("nyam", 10.0, 0.25));
+        products.add (new Chocolate("Mr.Brown", 10.0, "srawberry")); 
+        products.add (new Chocolate("Mr.Brown", 30.0, "blueberry"));
     }
 
     public Product getProductByName(String name){
         for (Product product : products) {
-            if(product.getName().equals(name))return product;
+            if(product.getName().equals(name))
+            System.out.println(product);
         }
         return null;
     }
     public Product getProductByCost(Double cost){
         for (Product product : products) {
-            if(product.getCost().equals(cost))return product;
+            if(product.getCost().equals(cost))
+            System.out.println(product);
         }
         return null;
     }
 
-    public BotlleOfTea getBotlleOfTea(String name, Double cost,Double volume){
+   /*  public BotlleOfTea getBotlleOfTea(String name, Double cost,Double volume){
         return new BotlleOfTea(name,cost,volume);
     }
     public Chocolate getChocolate(String name, Double cost, String taste){
@@ -42,7 +48,7 @@ public class VendingMachine {
     }
     public Cookieeeee getCookieeeee(String name, Double cost, Double weight){
         return new Cookieeeee(name,cost, weight);
-    }
+    }*/
 
 
     public Product getProductByCost(String string_cost){
